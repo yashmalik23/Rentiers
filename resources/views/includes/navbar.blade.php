69d0e29b401a3b4814a81737bd5c94de20fb1a01
Yash Malik <yashmalik23@gmail.com>
2019-09-08 16:28:48 +0530

    Slider fininshed

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="desktop-nav">
 
     {{-- Desktop Nav bar --}}
-    <img class="logo" src="{{asset('images/logo.svg')}}">
+    <img class="logo" src="{{asset('images/navbar/logo.svg')}}">
     <ul class="nav-links">
         <li><a href="#">Home</a></li>
         <li><a href="#">About Us</a></li>
@@ -10,7 +10,7 @@
         <li>|</li>
         <li>+91 9414573503</li>
         <li>
-            <img class="icons" src="{{asset('images/down-arrow.svg')}}"/>
+            <img class="icons" src="{{asset('images/navbar/down-arrow.svg')}}"/>
             <ul class="contact-numbers">
                 <li>+91 8683803539</li>
                 <li>+91 7901763826</li>
@@ -23,15 +23,15 @@
 <nav class="mobile-nav">
     {{-- Mobile nav bar --}}
     <div class="main-layout">
-        <img class="mobile-menu" src="{{asset('images/menu.svg')}}" onclick="handleMenu()">
-        <img class="mobile-logo" src="{{asset('images/logo.svg')}}">
+        <img class="mobile-menu" src="{{asset('images/navbar/menu.svg')}}" onclick="handleMenu()">
+        <img class="mobile-logo" src="{{asset('images/navbar/logo.svg')}}">
     </div>
     <ul class="mobile-nav-links">
         <li><a href="#">Home</a></li>
         <li><a href="#">About Us</a></li>
         <li><a href="#">Contact Us</a></li>
         <li><a href="#">List your property</a></li>
-        <li>+91 9414573503&nbsp;&nbsp;<img class="icons" src="{{asset('images/white-down-arrow.svg')}}"/></li>
+        <li>+91 9414573503&nbsp;&nbsp;<img class="icons" src="{{asset('images/navbar/white-down-arrow.svg')}}"/></li>
         <li><div class="mobile-login">Login/SignUp</div></li>
     </ul>
 </nav>
