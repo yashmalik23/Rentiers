0f1f9db443867b636b761d4f2fe0c43878bcbef6
Yash Malik <yashmalik23@gmail.com>
2019-09-09 13:06:08 +0530

    Footer done

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,29 @@
+<footer>
+    <img class="footer-left" src="{{asset('images/footer/footer-left.svg')}}">
+    <ul class="footer-link-columns">
+        <li>Recent Properties</li>
+        <li>Adani M2K Oyster Grande</li>
+        <li>Godrej Summut</li>
+        <li>The Meridion</li>
+        <li>Rental floor in house</li>
+        <li>Independent floor</li>
+    </ul>
+    <ul class="footer-link-columns">
+        <li>Recent Properties</li>
+        <li>Adani M2K Oyster Grande</li>
+        <li>Godrej Summut</li>
+        <li>The Meridion</li>
+        <li>Rental floor in house</li>
+        <li>Independent floor</li>
+    </ul>
+    <ul class="footer-link-columns">
+        <li>Recent Properties</li>
+        <li>Adani M2K Oyster Grande</li>
+        <li>Godrej Summut</li>
+        <li>The Meridion</li>
+        <li>Rental floor in house</li>
+        <li>Independent floor</li>
+    </ul>
+    <div class="copyright"> © rentiers.in, Gurugram, Haryana, India - 122003</div>
+    <img class="footer-right" src="{{asset('images/footer/footer-right.svg')}}">
+</footer>
